7ae004e64b074d64fd6ab1c24c1d14eab680b26d
FloweringVivian <flowering_season@163.com>
2017-06-09 15:16:00 +0800

    delete app.js

--- sense/js/app.js
+++ sense/js/app.js
@@ -1,10 +1,25 @@
 'use strict';
 
+var app = angular.module('app', ['ngRoute']);
 
-// Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+app.config(['$routeProvider', function($routeProvider){
+
+  $routeProvider
+      .when('/',{
+        controller: 'HomeController' ,
+        templateUrl: 'views/home.html'
+      })
+      .when('/schools',{
+        controller: 'AllSchoolsController',
+        templateUrl: 'views/allSchools.html'
+      })
+      .when('/classrooms',{
+        controller: 'AllClassroomsController',
+        templateUrl: 'views/allClassrooms.html'
+      })
+      .when('/activities',{
+        controller: 'AllActivitiesController',
+        templateUrl: 'views/allActivities.html'
+      })
+      .otherwise('/');
+}]);
