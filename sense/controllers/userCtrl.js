71be4c3591b182190e9a9141a472a07c600cedb3
FloweringVivian <flowering_season@163.com>
2017-06-12 21:39:08 +0800

    commit userList

--- sense/controllers/userCtrl.js
+++ sense/controllers/userCtrl.js
@@ -1,7 +1,10 @@
 "use strict";
 
-app.controller("userCtrl", function($scope){
-
+app.controller("userCtrl", function($scope, $http){
+    var url = 'http://localhost:63342/sense/json/userList.json';
+    $http.get(url, {"userName": ""}).success(function(data) {
+        $scope.userList = data.userList;
+    });
 });
 
 
