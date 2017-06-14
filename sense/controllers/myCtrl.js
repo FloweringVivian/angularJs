a0304a603592dfa77de01c416acb2e2d69a7dbb5
FloweringVivian <flowering_season@163.com>
2017-06-14 11:41:40 +0800

    my change

--- sense/controllers/myCtrl.js
+++ sense/controllers/myCtrl.js
@@ -1,8 +1,11 @@
 "use strict";
 
-app.controller("myCtrl", function($scope){
+app.controller("myCtrl", ["$scope", "user", function($scope, user){
     //title
     $("title").html("个人中心");
-});
+
+    $scope.userInfo = user.getUserInfo();
+
+}]);
 
 
