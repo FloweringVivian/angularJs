064aea6050b563039a0c57295b918cfc4622cd41
FloweringVivian <flowering_season@163.com>
2017-06-15 17:08:44 +0800

    directives commit

--- sense/controllers/productCtrl.js
+++ sense/controllers/productCtrl.js
@@ -1,8 +1,19 @@
 "use strict";
 
-app.controller("productCtrl", function($scope){
+app.controller("productCtrl", function($scope, $http){
     //title
     $("title").html("产品列表");
+
+    //产品列表
+    $scope.getProductList = function(){
+        var url = 'http://localhost:63342/sense/json/productList.json';
+        $http.post(url).success(function(data) {
+            $scope.productList = data.productList;
+        });
+    };
+
+    //初始化
+    $scope.getProductList();
 });
 
 
--- sense/engine/directives/userList.js
+++ sense/engine/directives/userList.js
@@ -7,7 +7,6 @@ app.directive('userList', [function () {
     replace: true,
     transclude: true,
     scope: {
-      "companyid": "=",
       "value": "=",
       "oldvalue": "=",
       "defval": "=",
@@ -21,22 +20,22 @@ app.directive('userList', [function () {
       "onChange": "&",
       "disabled": "@"
     },
-    templateUrl: "common/views/module/input/customer.html",
-    controller: ['$scope', 'rest', '$element', '$timeout', function ($scope, rest, $element, $timeout) {
-      $scope.pageSize = config.windowPageSize;
+    templateUrl: "views/directives/userList.html",
+    controller: ['$scope', '$http', '$element', '$timeout', function ($scope, $http, $element, $timeout) {
+      $scope.pageSize = 10;
       $scope.currentPage = 1;
       $scope.totalPage = 0;
 
-      $scope.selectAll = (_.isString($scope.all) && $scope.all.toLocaleLowerCase() == "true") ? true : false;
-      $scope.selectEmpty = (_.isString($scope.empty) && $scope.empty.toLocaleLowerCase() == "true") ? true : false;
-      $scope.isMulti = (_.isString($scope.multi) && $scope.multi.toLocaleLowerCase() == "false") ? false : true;
-      $scope.isDropdown = (_.isString($scope.dropdown) && $scope.dropdown.toLocaleLowerCase() == "false") ? false : true;
-      if (_.isString($scope.readonly) && $scope.readonly.toLocaleLowerCase() == "true") {
+      $scope.selectAll = (angular.isString($scope.all) && $scope.all.toLocaleLowerCase() == "true") ? true : false;
+      $scope.selectEmpty = (angular.isString($scope.empty) && $scope.empty.toLocaleLowerCase() == "true") ? true : false;
+      $scope.isMulti = (angular.isString($scope.multi) && $scope.multi.toLocaleLowerCase() == "false") ? false : true;
+      $scope.isDropdown = (angular.isString($scope.dropdown) && $scope.dropdown.toLocaleLowerCase() == "false") ? false : true;
+      if (angular.isString($scope.readonly) && $scope.readonly.toLocaleLowerCase() == "true") {
         $element.find('.dropdown-toggle > input').attr("readonly", "readonly");
       }
 
       $scope.$watch('disabled', function (nv, ov) {
-        if (_.isString($scope.disabled) && ($scope.disabled.toLocaleLowerCase() == "true" || $scope.disabled.toLocaleLowerCase() == "disabled")) {
+        if (angular.isString($scope.disabled) && ($scope.disabled.toLocaleLowerCase() == "true" || $scope.disabled.toLocaleLowerCase() == "disabled")) {
           $element.find('.dropdown-toggle').addClass("disabled");
         } else {
           $element.find('.dropdown-toggle').removeClass("disabled");
@@ -47,19 +46,19 @@ app.directive('userList', [function () {
         if (!(nv)) {
           return;
         }
-
-        rest.get('B01_findCustomerList', {
-          "companyId": $scope.companyid,
-          "pageSize": $scope.pageSize,
+        var url = 'http://localhost:63342/sense/json/userList.json';
+        var param = {
+          "searchKey": $scope.searchKey,
           "currentPage": $scope.currentPage,
-          "customerKey": ""
-        }, function (data) {
-          //log(data, 'B01_findCustomerList');
-          $scope.customerList = data.customerList;
+          "pageSize": $scope.pageSize
+        };
+        $http.post(url, param).success(function (data) {
 
-          _.forEach($scope.customerList, function (val) {
+          $scope.userList = data.userList;
 
-            if (val.customerId == nv) {
+          angular.forEach($scope.userList, function (val) {
+
+            if (val.id == nv) {
               $scope.select(val);
             }
           });
@@ -76,95 +75,34 @@ app.directive('userList', [function () {
 
       // 当输入框blur时，触发onBlur
       $scope.inputBlur = function () {
-        if (_.isFunction($scope.onBlur)) {
+        if (angular.isFunction($scope.onBlur)) {
           $scope.onBlur($scope.value);
         }
-      };
-
-      // 搜索
-      var keybak = "";
-      $scope.search = function () {
-        if ($scope.searchkey == keybak) return;
-        keybak = $scope.searchkey;
-
-        $scope.currentPage = 1;
-        queryData();
-      };
-
-      // 换页
-      $scope.goPage = function (pageNum) {
-        pageNum = parseInt(pageNum);
-        if (isNaN(pageNum) || pageNum > $scope.totalPage || pageNum < 1) return;
-
-        $scope.currentPage = pageNum;
-        queryData();
       };
 
       // 取得资料
       var queryData = function () {
         $scope.list = [];
-        rest.get('B01_findCustomerList', {
-          "companyId": $scope.companyid,
-          "pageSize": $scope.pageSize,
+        var url = 'http://localhost:63342/sense/json/userList.json';
+        var param = {
+          "searchKey": $scope.searchKey,
           "currentPage": $scope.currentPage,
-          "customerKey": keybak
-        }, function (data) {
+          "pageSize": $scope.pageSize
+        };
+        $http.post(url, param).success(function (data) {
           //log(data, 'B01_findCustomerList');
-          $scope.customerList = data.customerList;
-
+          $scope.userList = data.userList;
           $scope.totalPage = parseInt(data.totalPage ? data.totalPage : 1);
-
-          _.forEach($scope.customerList, function (val) {
-            if (selectTemp["key" + val.id]) {
-              val.sel = true;
-            }
-          });
-        });
-      };
-
-      // 点选单个checkbox
-      var selectTemp = {};
-      $scope.oncheck = function (item) {
-        $scope.selAll = true;
-        _.forEach($scope.customerList, function (val) {
-          if (!val.sel) {
-            $scope.selAll = false;
-          }
-        });
-        if (item.sel) {
-          selectTemp["key" + item.id] = item;
-        } else {
-          delete selectTemp["key" + item.id];
-        }
-      };
-      // 点选全选的checkbox
-      $scope.checkAll = function (sel) {
-        _.forEach($scope.customerList, function (val) {
-          val.sel = sel;
-          if (val.sel) {
-            selectTemp["key" + val.id] = val;
-          } else {
-            delete selectTemp["key" + val.id];
-          }
         });
       };
 
       // 点击下方选择button
       $scope.selectBtn = function (item) {
         $scope.oldvalue = $scope.value;
-        if (item) {
-          $scope.value = item;
-          $scope.name = item.buyCompName;
-        } else {
-          var temp = [];
-          _.forEach(selectTemp, function (val) {
-            if (val.sel) {
-              temp.push(val);
-            }
-          });
-          $scope.value = temp;
-        }
-        if (_.isFunction($scope.onChange)) {
+        $scope.value = item;
+        $scope.name = item.userName;
+
+        if (angular.isFunction($scope.onChange)) {
           $timeout($scope.onChange);
         }
         $element.find('.modal').modal("hide");
@@ -174,62 +112,9 @@ app.directive('userList', [function () {
       $scope.select = function (item) {
         $scope.oldvalue = $scope.value;
         $scope.value = item;
-        $scope.name = item.buyCompName;
+        $scope.name = item.userName;
       };
 
-      // 下拉选单选择全部
-      $scope.select_all = function (e) {
-        // 未实现
-      };
-
-      // 下拉选择空
-      $scope.select_empty = function () {
-        $scope.value = "";
-        $scope.name = "";
-      };
-      // 只能输入正数
-      $scope.mustPlus = function () {
-        if (!$scope.pageNum) {
-          $scope.pageNum = 1;
-          return;
-        }
-        if (isNaN($scope.pageNum * 1)) {
-          $scope.pageNum = 1;
-          return;
-        }
-        if ($scope.pageNum < 1) {
-          $scope.pageNum = 1;
-          return;
-        }
-      };
-
-      // 输入框内容变更时
-      //var oldName;
-      $scope.change = function (e) {
-
-        // 没对应的接口
-        // if (_.trim($scope.name) == "") {
-        //     $scope.list = $scope.response;
-        //     return;
-        // }
-        //
-        // if ($scope.name == oldName) return;
-        //
-        // var self = $(e.target);
-        // var parent = self.parent().parent();
-        // parent.addClass("open");
-        //
-        // oldName = $scope.name;
-        //
-        // $scope.list = [];
-        // rest.get('B01_getVendorInfoByCode', {
-        //     "companyId": $scope.companyid,
-        //     "vendorKey": $scope.name
-        // }, function (data) {
-        //     //log(data, 'B01_getVendorInfoByCode');
-        //     $scope.list = data.customerList;
-        // });
-      };
     }]
   };
 }]);
